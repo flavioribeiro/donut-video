8472851e6f8598f1adf90b72b25509307188380f
Flavio Ribeiro <flavior@netflix.com>
2022-12-01 08:28:10 -0500

    small refactory on connection manager

--- src/connection-manager.js
+++ src/connection-manager.js
@@ -17,8 +17,37 @@ class ConnectionManager {
         };
     }
 
-    connect(server, src) {
+    buildPayload(offer, src) {
         let source = this.parseSource(src);
+        return JSON.stringify({
+            "srtHost": source.host,
+            "srtPort": source.port,
+            "srtStreamId": source.streamId,
+            offer
+        });
+    }
+
+    async fetchRemoteDescription(server, src, offer) {
+        const signalingUrl = server + '/doSignaling';
+        return fetch(signalingUrl, {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: this.buildPayload(offer, src)
+        }).then(res => {
+            if (res.status !== 200) {
+                res.text().then(err => {
+                    window.alert(err)
+                })
+                return;
+            }
+            return res.json()
+        })
+    }
+
+    connect(server, src) {
         this.peerConnection = new RTCPeerConnection();
         this.peerConnection.addTransceiver('video', { direction: 'recvonly' })
         this.peerConnection.ontrack = (event) => {
@@ -28,30 +57,7 @@ class ConnectionManager {
         this.peerConnection.createOffer()
             .then(offer => {
                 this.peerConnection.setLocalDescription(offer)
-                const signalingUrl = server + '/doSignaling';
-                return fetch(signalingUrl, {
-                    method: 'post',
-                    headers: {
-                        'Accept': 'application/json, text/plain, */*',
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        "srtHost": source.host,
-                        "srtPort": source.port,
-                        "srtStreamId": source.streamId,
-                        offer
-                    })
-                })
-            })
-            .then(res => {
-                if (res.status !== 200) {
-                    res.text().then(err => {
-                        window.alert(err)
-                    })
-                    return;
-                }
-
-                return res.json()
+                return this.fetchRemoteDescription(server, src, offer);
             })
             .then(res => {
                 this.peerConnection.setRemoteDescription(res)
